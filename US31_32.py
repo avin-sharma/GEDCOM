5703afd212874d36ba6aee67cb56a8e5d6479ae2
jaygoyani1 <goyanijay@gmail.com>
2020-03-22 17:45:40 -0400

    mysprint2

---
+++ US31_32.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from prettytable import PrettyTable
+
+
+def multiple_birth(individuals, families, tag_positions):
+    warnings = []
+    dic = {}
+    for indi_id in individuals:
+        individual = individuals[indi_id]
+        if(individual.birth != None):
+            dic[individual.name] = individual.birth
+    new_dict = {}
+    for pair in dic.items():
+        if pair[1] not in new_dict.keys():
+            new_dict[pair[1]] = []
+        new_dict[pair[1]].append(pair[0])
+    for k, v in new_dict.items():
+        if (len(v) >= 2):
+            warnings.append(
+                f'ANOMALY: FAMILY: US32, {v} The two or more individuals were born at the same time')
+    return warnings
+
+
+def listLivingSingle(individuals, families, tag_positions):
+    warning = []
+    for indi_id in individuals:
+        individual = individuals[indi_id]
+        if(individual.age != None and int(individual.age) > 30 and individual.death == None and len(individual.spouse) == 0):
+            warning.append(
+                f'{individual.name} is over 30 years and still not married')
+    return warning
